882e08a1d16ce4d7a22312c319f3bfe071ee1324
Joud Shaieb <joudshaieb524@gmail.com>
2025-03-23 02:35:00 +0400

    dev: add cookies auth, cors and static files to middleware

--- Program.cs
+++ Program.cs
@@ -1,34 +1,50 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
+using MailReader.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
-//builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
+builder.Services.AddSingleton<ITokenStorageService, FileTokenStorageService>();
+
+// Add Cookie Authentication
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
     {
-        options.Authority = "https://accounts.google.com";
-        options.Audience = builder.Configuration["Authentication:Google:ClientId"];
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = "https://accounts.google.com",
-            ValidateAudience = true,
-            ValidAudience = builder.Configuration["Authentication:Google:ClientId"],
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-        };
+        options.LoginPath = "/auth";
+        options.LogoutPath = "/auth/logout";
+        options.Cookie.Name = "EmailReaderAuth";
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SameSite = SameSiteMode.Lax;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
     });
 
 builder.Services.AddAuthorization();
 builder.Services.AddHttpClient();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocal", builder =>
+    {
+        builder.WithOrigins("https://localhost:5001", "http://localhost:5000")
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials();
+    });
+});
+
+// Add static files support
+builder.Services.AddDirectoryBrowser();
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowLocal");
+
+// Serve static files from the wwwroot folder
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseAuthentication();
 app.UseAuthorization();
